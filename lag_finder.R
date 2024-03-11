01a16c1fe8539254efc37d9bae16fccac5fc6f40
Billy Mitchell <62663636+wj-mitchell@users.noreply.github.com>
2024-03-11 11:51:52 -0400

    Update lag_finder.R
    Removing the need for dataframes to be submitted

--- lag_finder.R
+++ lag_finder.R
@@ -1,9 +1,7 @@
 lag_finder <- function(lags = seq(-40,40,1), # all of the possible lags to test
-                       data_x, # The dataframe that this data exists within
-                       data_y, # The dataframe that this data exists within
                        time, # The time variable which you wish to apply the lags to
-                       x, # The variable to add a time-shift and correlate with your y variable
-                       y, # The variable to correlate with your x variable
+                       x, # The data to add a time-shift and correlate with your y variable
+                       y, # The data to correlate with your x variable
                        window_size = NA, # the size of the sliding window that we should apply  
                        method = "spearman") # Which method that you want cor() to use
 { 
@@ -11,11 +9,11 @@ lag_finder <- function(lags = seq(-40,40,1), # all of the possible lags to test
   
   # ----- QA CHECKS -----
   
-  if (length(data_x[[x]]) != length(data_y[[y]])){
+  if (length(x) != length(y)){
     stop("The length of x and y do not match. Please submit two arrays of equal length and try again")
   }
   
-  if (any(!is.numeric(data_x[[x]]) | !is.numeric(data_y[[y]]))){
+  if (any(!is.numeric(x) | !is.numeric(y))){
     stop("X and Y must be numeric arrays of equal length. Please consider coercing or reformatting your values and try again")
   }
   
@@ -23,8 +21,8 @@ lag_finder <- function(lags = seq(-40,40,1), # all of the possible lags to test
     stop("Window size must be NA OR a numeric value greater than 1. Please correct this and try again")
   }
   
-  if (!is.numeric(data_x[[time]]) | !is.numeric(data_y[[time]])){
-    stop("Time must be a numerically structured variable in dataframes x and y. Please try coerecing them into the correct format and try again")
+  if (!is.numeric(time)){
+    stop("Time must be a numerically structured array. Please try coerecing them into the correct format and try again")
   }
   
   if (!is.numeric(lags) | length(lags) < 2){
@@ -75,44 +73,42 @@ lag_finder <- function(lags = seq(-40,40,1), # all of the possible lags to test
                                    dimnames = list(paste0("Win_",1:rows),
                                                    paste0("Lag_", lags))))
   }
-  
-  # Noting which column is the time column
-  col_time <- which(names(data_x) == time)
-  
+    
   # Noting time mins and maxes
-  max_time <- max(data_x[,col_time])
-  min_time <- min(data_x[,col_time])
+  max_time <- max(time)
+  min_time <- min(time)
+
+  # Creating a dataframe to house the Y variables
+  data_y <- data.frame(time = time,
+                       y = y)
   
   # Iterating through all possible lags
   for (LAG in 1:length(lags)){
     
     # Creating scratch dataframes to work within
-    data_temp_x <- data_x
+    data_temp_x <- data.frame(time = time,
+                              x = x)
     
     # Adding the lag to the behavioral data
-    data_temp_x[,col_time] <- data_temp_x[,col_time] + lags[LAG]
+    data_temp_x[["time"]] <- data_temp_x[["time"]] + lags[LAG]
     
     # If there actually was a lag
     if (lags[LAG] != 0){
       
       # Shaving off any observations that now fall outside of the Window range
-      data_temp_x <- data_temp_x[-which(data_temp_x[,col_time] < min_time | data_temp_x[,col_time] > max_time),] 
+      data_temp_x <- data_temp_x[-which(data_temp_x[["time"]] < min_time | data_temp_x[["time"]] > max_time),] 
     }
     
     # Merging out behavioral and neural data together
     df <- merge(data_temp_x, 
                 data_y)
     
-    # Noting which column are the correlation columns
-    col_x <- which(names(df) == x)
-    col_y <- which(names(df) == y)
-    
     # If window has no value
     if (is.na(window_size)){
       
       # Generating a correlative value for this region
-      values[LAG] <- cor(df[,col_x], 
-                         df[,col_y], 
+      values[LAG] <- cor(df[[x]], 
+                         df[[y]], 
                          method = method)
     }
     
@@ -120,8 +116,8 @@ lag_finder <- function(lags = seq(-40,40,1), # all of the possible lags to test
     if (!is.na(window_size)){
       
       # Generate a series of correlative value for this region
-      values[,LAG] <- Sliding_Window_Cor(x = df[,col_x], 
-                                         y = df[,col_y],
+      values[,LAG] <- Sliding_Window_Cor(x = df[[x]], 
+                                         y = df[[y]],
                                          window_size = window_size,
                                          cor_method = method)
       
