cf84a30a095cd5ffc818058e9c713f310d779d09
SANlab <tui81100@temple.edu>
2024-12-23 19:42:12 -0500

    Update GenOnsets.R

--- GenOnsets.R
+++ GenOnsets.R
@@ -1,7 +1,7 @@
 GenOnsets <- function(PIDs,  # An array of participant IDs to Process
                       Tasks = c("3_task-1", "5_task-2"), # An array of the different run name(s) that appear on the DICOM files
                       RawDir = "/data/Social_Regulation/3_data/neuro/raw/", # The directory in which your DICOM files are stored
-                      BehavDir = "/data/Social_Regulation/3_data/neuro/behav/", # The directory in which your MRI behavioral data is stored
+                      BehavDir = "/data/Social_Regulation/3_data/behavior/raw/", # The directory in which your MRI behavioral data is stored
                       DerivDir = "/data/Social_Regulation/3_data/neuro/deriv/pipeline_1/fmriprep/", # The directory in which your preprocessed data is stored
                       TR = 2, # The length of your repetition time in seconds
                       ShaveLength = 17, # How much time should be shaved from the beginning of your data in seconds
